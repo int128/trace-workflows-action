ccd9d3d985a0fef2f3433cb530a8463f1e2de31d
Hidetake Iwata <int128@gmail.com>
2024-09-12 16:14:59 +0900

    Fix test

---
+++ tests/checks.test.ts
@@ -0,0 +1,77 @@
+import { summaryListChecksQuery } from '../src/checks.js'
+import { CheckConclusionState, CheckStatusState } from '../src/generated/graphql-types.js'
+
+describe('summaryListChecksQuery', () => {
+  it('should return a summary of workflow runs', () => {
+    const workflowRuns = summaryListChecksQuery({
+      __typename: 'Query',
+      rateLimit: {
+        cost: 1,
+        remaining: 4999,
+      },
+      repository: {
+        __typename: 'Repository',
+        object: {
+          __typename: 'Commit',
+          checkSuites: {
+            __typename: 'CheckSuiteConnection',
+            pageInfo: {
+              endCursor: 'cursor',
+              hasNextPage: false,
+            },
+            totalCount: 1,
+            nodes: [
+              {
+                __typename: 'CheckSuite',
+                workflowRun: {
+                  __typename: 'WorkflowRun',
+                  event: 'push',
+                  workflow: {
+                    __typename: 'Workflow',
+                    name: 'CI',
+                  },
+                },
+                createdAt: '2021-08-04T00:00:00Z',
+                status: CheckStatusState.Completed,
+                conclusion: CheckConclusionState.Success,
+                checkRuns: {
+                  __typename: 'CheckRunConnection',
+                  totalCount: 1,
+                  nodes: [
+                    {
+                      __typename: 'CheckRun',
+                      name: 'build',
+                      status: CheckStatusState.Completed,
+                      conclusion: CheckConclusionState.Success,
+                      startedAt: '2021-08-04T00:00:00Z',
+                      completedAt: '2021-08-04T00:01:00Z',
+                    },
+                  ],
+                },
+              },
+            ],
+          },
+        },
+      },
+    })
+    expect(workflowRuns).toEqual([
+      {
+        event: 'push',
+        workflowName: 'CI',
+        status: CheckStatusState.Completed,
+        conclusion: CheckConclusionState.Success,
+        createdAt: new Date('2021-08-04T00:00:00Z'),
+        completedAt: new Date('2021-08-04T00:01:00Z'),
+        jobs: [
+          {
+            name: 'build',
+            status: CheckStatusState.Completed,
+            conclusion: CheckConclusionState.Success,
+            startedAt: new Date('2021-08-04T00:00:00Z'),
+            completedAt: new Date('2021-08-04T00:01:00Z'),
+          },
+        ],
+      },
+    ])
+  })
+})
