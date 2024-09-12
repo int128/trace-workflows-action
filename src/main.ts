508647b24aeeda990ec25997a9f6e7815455eabf
Hidetake Iwata <int128@gmail.com>
2024-09-12 13:54:39 +0900

    Add opentelemetry tracer (#3)
    * Add opentelemetry tracer
    
    * Add enable-oltp
    
    * Fix

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,32 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 import { run } from './run.js'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
+import { ConsoleSpanExporter } from '@opentelemetry/sdk-trace-node'
 
 const main = async (): Promise<void> => {
-  await run({
-    owner: github.context.repo.owner,
-    repo: github.context.repo.repo,
-    sha: core.getInput('sha', { required: true }),
-    token: core.getInput('token', { required: true }),
+  const enableOTLP = core.getBooleanInput('enable-oltp', { required: true })
+  const sdk = new NodeSDK({
+    traceExporter: enableOTLP ? new OTLPTraceExporter() : new ConsoleSpanExporter(),
   })
+  sdk.start()
+
+  try {
+    await run({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      sha: core.getInput('sha', { required: true }),
+      token: core.getInput('token', { required: true }),
+    })
+  } finally {
+    await core.group('Shutting down OpenTelemetry', async () => {
+      await sdk.shutdown()
+    })
+  }
 }
 
-main().catch((e: Error) => {
+await main().catch((e: Error) => {
   core.setFailed(e)
   console.error(e)
 })
