b87e1b342c044b35f690e40c82df7ae394141e55
Hidetake Iwata <int128@gmail.com>
2024-09-14 17:28:33 +0900

    Refactor (#25)

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,18 @@
 import * as core from '@actions/core'
 import { run } from './run.js'
-import { NodeSDK } from '@opentelemetry/sdk-node'
-import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto'
-import { ConsoleSpanExporter, SpanExporter } from '@opentelemetry/sdk-trace-node'
+import { withOpenTelemetry } from './opentelemetry.js'
 
-const main = async (): Promise<void> => {
-  let traceExporter: SpanExporter = new ConsoleSpanExporter()
-  const oltpEndpoint = core.getInput('oltp-endpoint')
-  if (oltpEndpoint) {
-    traceExporter = new OTLPTraceExporter({
-      url: oltpEndpoint,
-    })
-  }
-  const sdk = new NodeSDK({ traceExporter })
-  sdk.start()
-
-  try {
-    await run({
-      token: core.getInput('token', { required: true }),
-    })
-  } finally {
-    await core.group('Shutting down OpenTelemetry', async () => {
-      await sdk.shutdown()
-    })
-  }
-}
+const main = async () =>
+  withOpenTelemetry(
+    {
+      endpoint: core.getInput('oltp-endpoint'),
+    },
+    async () => {
+      await run({
+        token: core.getInput('token', { required: true }),
+      })
+    },
+  )
 
 await main().catch((e: Error) => {
   core.setFailed(e)
