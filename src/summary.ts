df2d8ec892f50cb5536d87163edd3461ac9f5fac
Hidetake Iwata <int128@gmail.com>
2026-07-20 13:47:30 +0900

    Respect conclustion

--- src/summary.ts
+++ src/summary.ts
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import type { WorkflowEvent } from './checks.js'
+import { CheckConclusionState } from './generated/graphql-types.js'
 
 export const writeTraceSummary = async (event: WorkflowEvent) => {
   core.summary.addHeading('trace-workflows summary', 2)
@@ -17,21 +18,38 @@ export const writeTraceSummary = async (event: WorkflowEvent) => {
       for (const job of workflowRun.jobs) {
         if ((job.steps?.length ?? 0) > 0) {
           for (const step of job.steps ?? []) {
+            const tag = getTag(step.conclusion)
             const seconds = (step.completedAt.getTime() - step.startedAt.getTime()) / 1000
             lines.push(
-              `${workflowRun.workflowName} / ${job.name} / ${step.name} : ${step.startedAt.toISOString()}, ${seconds}s`,
+              `${workflowRun.workflowName} / ${job.name} / ${step.name} :${tag}, ${step.startedAt.toISOString()}, ${seconds}s`,
             )
           }
         } else {
+          const tag = getTag(job.conclusion)
           const seconds = (job.completedAt.getTime() - job.startedAt.getTime()) / 1000
-          lines.push(`${workflowRun.workflowName} / ${job.name} : ${job.startedAt.toISOString()}, ${seconds}s`)
+          lines.push(`${workflowRun.workflowName} / ${job.name} :${tag}, ${job.startedAt.toISOString()}, ${seconds}s`)
         }
       }
     } else {
+      const tag = getTag(workflowRun.conclusion)
       const seconds = (workflowRun.completedAt.getTime() - workflowRun.createdAt.getTime()) / 1000
-      lines.push(`${workflowRun.workflowName} : ${workflowRun.createdAt.toISOString()}, ${seconds}s`)
+      lines.push(`${workflowRun.workflowName} :${tag}, ${workflowRun.createdAt.toISOString()}, ${seconds}s`)
     }
   }
   core.summary.addCodeBlock(lines.join('\n'), 'mermaid')
   await core.summary.write()
+}
+
+const getTag = (conclusion: CheckConclusionState | undefined): string => {
+  switch (conclusion) {
+    case CheckConclusionState.Failure:
+    case CheckConclusionState.Cancelled:
+    case CheckConclusionState.TimedOut:
+    case CheckConclusionState.StartupFailure:
+      return 'critical'
+    case CheckConclusionState.Skipped:
+      return 'done'
+    default:
+      return ''
+  }
 }
